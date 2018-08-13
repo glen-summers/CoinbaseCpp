28fab63ab0fdcb6a047880c0f94af162bac117e3
Glen Summers <glen.summers@outlook.com>
2018-08-13 19:01:34 +0100

    shallow and sparse dependencies

---
+++ init.cmd
@@ -0,0 +1,14 @@
+::init sparse checkout
+@echo off
+setlocal
+cd %~dp0
+
+git config core.sparsecheckout true || (echo git config failed & exit /b 1)
+git submodule init || (echo submodule init & exit /b 1)
+
+md .git\modules\Dependencies\rapidjson\info || (echo md failed & exit /b 1)
+
+echo include > .git\modules\Dependencies\rapidjson\info\sparse-checkout || (echo sparse write failed & exit /b 1)
+git read-tree -mu HEAD || (echo git read tree failed & exit /b 1)
+
+exit /b 0
