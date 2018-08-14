a2f6ef1b83ca74696a011327a457b66e651b8aa6
Glen Summers <glen.summers@outlook.com>
2018-08-14 17:07:33 +0100

    fix(hack) init script

--- init.cmd
+++ init.cmd
@@ -1,14 +1,16 @@
 ::init sparse checkout
+::only way I found to get this to work is to [init, update, deinit], this leaves the module config in .git\modules\...
+:: then inject the sparse config and init, update
 @echo off
 setlocal
 cd %~dp0
 
-git config core.sparsecheckout true || (echo git config failed & exit /b 1)
-git submodule init || (echo submodule init & exit /b 1)
-
-if not exist .git\modules\Dependencies\rapidjson\info md .git\modules\Dependencies\rapidjson\info || (echo md failed & exit /b 1)
-
+git submodule init || (echo submodule init failed & exit /b 1)
+git submodule update || (echo submodule update failed & exit /b 1)
+git submodule deinit --all || (echo submodule deinit failed & exit /b 1)
+git -C .git\modules\Dependencies\rapidjson config core.sparsecheckout true || (echo config sparse failed & exit /b 1)
 echo include > .git\modules\Dependencies\rapidjson\info\sparse-checkout || (echo sparse write failed & exit /b 1)
-git read-tree -mu HEAD || (echo git read tree failed & exit /b 1)
+git submodule init || (echo submodule init failed & exit /b 1)
+git submodule update || (echo submodule update failed & exit /b 1)
 
 exit /b 0
