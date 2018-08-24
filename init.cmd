ad67357d20ed8f52c3035e73df93379c39557d8b
Glen Summers <glen.summers@outlook.com>
2018-08-24 01:55:39 +0100

    avoid: init,deinit,set sparse,update use:   update\init(NOP),set sparse, update

--- init.cmd
+++ init.cmd
@@ -1,12 +1,11 @@
 :: init sparse checkout
-:: only way I found so far to get sparse submodule checkout to work is to [init, update, deinit], this leaves the module config in .git\modules\...
-:: then inject the sparse config and rerun [init, update]
-@echo off
+:: one way I found to get sparse submodule checkout to work is to [init, update, deinit], this leaves the module config in .git\modules\... then inject the sparse config and rerun [init, update]
+:: an alternative is to update --init with a bogus cmd this creates the .git module info but no update, then set the sparse data and then do a real update
+::@echo off
 setlocal
 cd %~dp0
 
-git submodule update --init || (echo submodule update failed & exit /b 1)
-git submodule deinit --all || (echo submodule deinit failed & exit /b 1)
+git -c submodule.Dependencies/rapidjson.update=^!echo submodule update --init || (echo submodule update failed & exit /b 1)
 git -C .git\modules\Dependencies\rapidjson config core.sparsecheckout true || (echo config sparse failed & exit /b 1)
 echo include > .git\modules\Dependencies\rapidjson\info\sparse-checkout || (echo sparse write failed & exit /b 1)
 git submodule update --init || (echo submodule update failed & exit /b 1)
